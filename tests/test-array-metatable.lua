873eeb9a271a82a7c8201e4a2315b4fd50ffcca5
Christopher Moore <christopher.e.moore@gmail.com>
2026-05-07 18:16:13 +0700

    made assert errors where there should be

--- tests/test-array-metatable.lua
+++ tests/test-array-metatable.lua
@@ -24,11 +24,42 @@ assert.eq(3*3+1, a:test())
 a.a = 4
 assert.eq(4*4+1, A.test(a))
 
--- now for array types ...
+-- [[ now for array types ... this fails in luajit
 local A4 = ffi.typeof('$[4]', A)
 print(A4)
-print(A4.test)
-assert.eq(A4.test, A.test)
+assert.error(function()
+	-- this should error because it __indexes a field in a ctype that isn't there
+	-- and in luajit it is a error-worthy offense to __index a cdata/ctype field that isn't there.
+	print(A4.test)
+end)
+assert.error(function()
+	assert.eq(A4.test, A.test)
+end)
 local a4 = A4()
-print(a4.test)
-assert.eq(a4.test, A.test)
+assert.error(function()
+	print(a4.test)
+end)
+assert.error(function()
+	-- this will pass in bugged luaffifb because it gives array types their ctype metatables
+	assert.eq(a4.test, A.test)
+end)
+--]]
+
+
+local mt4
+assert.error(function()
+	-- luajit gives: "bad argument #1 to 'metatype' (invalid C type)"
+	-- so luajit can't metatype an array type (which I think is good)
+	mt4 = ffi.metatype(A4, {
+		__index = {
+			test2 = function(self)
+				print('can luajit give array types separate metatypes?')
+				return 43
+			end,
+		}
+	})
+end)
+assert.error(function()
+	-- all these pass in bugged luaffifb
+	print(A4.test2)
+end)

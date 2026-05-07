ff6a6f592adfaa036576151c0a57e0e9f45196fa
Christopher Moore <christopher.e.moore@gmail.com>
2026-05-07 20:48:52 +0700

    more

--- tests/typeparams.lua
+++ tests/typeparams.lua
@@ -1,5 +1,6 @@
 #!/usr/bin/env luajit
 local ffi = require 'ffi'
+local assert = require 'ext.assert'
 
 -- make sure type-params work
 
@@ -13,18 +14,23 @@ local uint16_t = ffi.typeof'uint16_t'
 local uint32_t = ffi.typeof'uint32_t'
 local uint64_t = ffi.typeof'uint64_t'
 
-assert(int == int32_t)
-assert(ffi.sizeof(int) == 4)
-assert(ffi.typeof(int) == int)
+assert.eq(int, int32_t)
+assert.eq(ffi.sizeof(int), 4)
+assert.eq(ffi.typeof(int), int)
 
 local int_2nd = ffi.typeof('$', int)
-assert(int == int_2nd)
+assert.eq(int, int_2nd)
 
 -- make sure structs can use them
-local int8_1 = ffi.typeof('$[1]', int8_t)	-- use arrays since the numbers themselves would be converted to lua numbers
-local int16_1 = ffi.typeof('$[1]', int16_t)	-- use arrays since the numbers themselves would be converted to lua numbers
-local int32_1 = ffi.typeof('$[1]', int32_t)	-- use arrays since the numbers themselves would be converted to lua numbers
-local int64_1 = ffi.typeof('$[1]', int64_t)	-- use arrays since the numbers themselves would be converted to lua numbers
+-- use arrays since the numbers themselves would be converted to lua numbers
+local int8_1 = ffi.typeof('$[1]', int8_t)
+local int8_ref_1 = ffi.typeof('$(&)[1]', int8_t)	-- TODO support for refs-to-arrays in luaffifb...
+local int16_1 = ffi.typeof('$[1]', int16_t)
+local int16_ref_1 = ffi.typeof('$(&)[1]', int16_t)
+local int32_1 = ffi.typeof('$[1]', int32_t)
+local int32_ref_1 = ffi.typeof('$(&)[1]', int32_t)
+local int64_1 = ffi.typeof('$[1]', int64_t)
+local int64_ref_1 = ffi.typeof('$(&)[1]', int64_t)
 
 ffi.cdef([[
 typedef struct {
@@ -36,10 +42,10 @@ typedef struct {
 local A = ffi.typeof'A'
 
 local a = A()
-assert(ffi.typeof(a) == A)
-assert(ffi.typeof(a.a), int16_1)
-assert(ffi.typeof(a.b), int64_1)
-assert(ffi.sizeof(A) == 16)
+assert.eq(ffi.typeof(a), A)
+assert.eq(ffi.typeof(a.a), int16_ref_1)
+assert.eq(ffi.typeof(a.b), int64_ref_1)
+assert.eq(ffi.sizeof(A), 16)
 
 -- try again but with anonymous type
 local B = ffi.typeof([[struct {
@@ -48,14 +54,14 @@ local B = ffi.typeof([[struct {
 }]], int8_1, int32_1)
 
 local b = B()
-assert(ffi.typeof(b) == B)
-assert(ffi.typeof(b.a), int8_1)
-assert(ffi.typeof(b.b), int32_1)
-assert(ffi.sizeof(B) == 8)
+assert.eq(ffi.typeof(b), B)
+assert.eq(ffi.typeof(b.a), int8_ref_1)
+assert.eq(ffi.typeof(b.b), int32_ref_1)
+assert.eq(ffi.sizeof(B), 8)
 
 local C = ffi.typeof([[struct { $ a, b; $ c, d; }]], A, B)
-assert(ffi.offsetof(C, 'a') == 0)
-assert(ffi.offsetof(C, 'b') == 16)
-assert(ffi.offsetof(C, 'c') == 32)
-assert(ffi.offsetof(C, 'd') == 40)
-assert(ffi.sizeof(C) == 48)
+assert.eq(ffi.offsetof(C, 'a'), 0)
+assert.eq(ffi.offsetof(C, 'b'), 16)
+assert.eq(ffi.offsetof(C, 'c'), 32)
+assert.eq(ffi.offsetof(C, 'd'), 40)
+assert.eq(ffi.sizeof(C), 48)
